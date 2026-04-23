f33baf4763f60ce039148b86dd25485adc8c443a
carlos.lima <carlos.lime@jcatlm.com>
2026-04-23 16:38:18 -0300

    test: Start test cases and config file use

---
+++ e2e/search_by_something.spec.ts
@@ -0,0 +1,6 @@
+import { test, expect } from '@playwright/test';
+
+test("Should search by something on search_bar", async ({ page }) => {
+	// Wip: I need to write a simple test to check
+	// if config file are consumed by app.
+})
